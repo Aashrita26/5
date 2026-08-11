8d1852dbd2432d1a6b732a7e0b4d10f05b8204ec
Aashrita <aashrita.nadge@gmail.com>
2026-08-11 22:30:22 +0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -7,16 +7,11 @@ images[i].setAttribute("tabindex", "0");
 window.onload = addTabIndex;
 function upDate(previewPic) {
 let imageDiv = document.getElementById("image");
-
 imageDiv.style.backgroundImage = `url('${previewPic.src}')`;
-
 imageDiv.innerHTML = previewPic.alt;
 }
 function unDo() {
-
 let imageDiv = document.getElementById("image");
-
 imageDiv.style.backgroundImage = "";
-
 imageDiv.innerHTML = "Hover over an image below to display here.";
 }
